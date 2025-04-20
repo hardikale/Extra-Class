33d28714d1dae83cb54ad4e79ad78b22e8542537
hardikale <mehardikale@gmail.com>
2025-04-20 07:45:23 +0545

    notices connected to db(sql)

--- admin/actionnotice.php
+++ admin/actionnotice.php
@@ -1,7 +1,17 @@
 <?php
 if(isset($_POST['store']))
 {
-    $notcie = $_POST['notice'];
-    echo $notcie;
+    $notice = $_POST['notice'];
+    $qry = "INSERT INTO notices (notice) VALUES ('$notice')";
+    include 'dbconnection.php';
+    $result = mysqli_query($conn,$qry);
+    if ($result) {
+        echo "<script>alert('notice created successfully');</script>";
+        echo "<script>window.location.href='notice.php';</script>";
+    } else {
+        echo "Failed to create Notcie.";
+    }
+
+    include 'closeconnection.php';
 }
 ?>
