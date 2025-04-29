089e740d313dac9a0b30c699310da9f68ee1112f
hardikale <mehardikale@gmail.com>
2025-04-29 06:57:12 +0545

    dynamic total count

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,4 +1,13 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php'; 
+$qrycat = "SELECT COUNT(id) as total_cat FROM categories";
+$qrynotice = "SELECT COUNT(id) as total_notice FROM notices";
+include 'dbconnection.php';
+$resultcat = mysqli_query($conn, $qrycat);
+$resultnotice = mysqli_query($conn, $qrynotice);
+$rowcat = mysqli_fetch_assoc($resultcat);
+$rownotice = mysqli_fetch_assoc($resultnotice);
+
+?>
             <h2 class="text-3xl font-semibold py-4">Dashboard</h2>
             <hr class="mb-4 h-1 bg-red-600">
         <div class="grid grid-cols-3 gap-5">
@@ -8,11 +17,11 @@
             </div>
             <div class="bg-red-100 px-4 py-6 rounded-lg shadow-md">
              <h3 class="text-xl">Total Categories</h3>
-             <p class="text-3xl font-semibold text-right">100</p>
+             <p class="text-3xl font-semibold text-right"><?php echo $rowcat['total_cat']; ?></p>
             </div>
             <div class="bg-yellow-100 px-4 py-6 rounded-lg shadow-md">
              <h3 class="text-xl">Total Notices</h3>
-             <p class="text-3xl font-semibold text-right">100</p>
+             <p class="text-3xl font-semibold text-right"><?php echo $rownotice['total_notice']; ?></p>
             </div>
         </div>
 <?php include 'footer.php'; ?>
--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+<?php 
+    $qry = "SELECT * FROM categories ORDER BY priority";
+    include 'admin/dbconnection.php';
+    $resultcat = mysqli_query($conn, $qry);
+    include 'admin/closeconnection.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,8 +17,10 @@
         <h2 class="font-bold text-2xl">LOGO</h2>
         <div class="flex gap-6 items-center">
             <a href="index.php" class="text-gray-600 hover:scale-105 transition duration-100">Home</a>
-            <a href="" class="text-gray-600 hover:scale-105 transition duration-100">About</a>
-            <a href="" class="text-gray-600 hover:scale-105 transition duration-100">Contact</a>
+            <?php while ($rowcat = mysqli_fetch_assoc($resultcat)) { ?>
+               <a href="" class="text-gray-600 hover:scale-105 transition duration-100"><?php echo $rowcat['name'] ?></a>
+            <?php } ?>
+            
             <div class="border-0 px-4 py-1 rounded-full bg-slate-800 hover:bg-slate-950 hover:scale-95 transition duration-100"><a href="login.php" class="text-white text-sm  font-semibold">LOGIN</a>
             </div>
         </div>
